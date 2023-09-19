dc522ce81c2f8db307b11e1396ab82fdfd6c3bcd
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-09-19 15:58:44 +0700

    Update auto create superadmin

--- backend/vending_machine/runserver.sh
+++ backend/vending_machine/runserver.sh
@@ -1,4 +1,4 @@
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 
 # python manage.py migrate
@@ -6,4 +6,7 @@ python manage.py makemigrations
 python manage.py migrate --run-syncdb
 # python manage.py compilemessages -l th
 
+python manage.py createsuperuser --noinput --email "admin@mail.com"
+
 python manage.py runserver 0.0.0.0:8000
+
