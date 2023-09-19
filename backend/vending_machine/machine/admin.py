256842d9ee8efce90cf27c04889cfc2b05f7faad
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-09-19 18:56:33 +0700

    Update model

--- backend/vending_machine/machine/admin.py
+++ backend/vending_machine/machine/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import VendingMachine
+from .models import VendingMachine, ItemsInMachine
 
 
 @admin.register(VendingMachine)
 class VendingMachineAdmin(admin.ModelAdmin):
     search_fields = [
         'code', 'name',
+    ]
+
+
+@admin.register(ItemsInMachine)
+class ItemsInMachineAdmin(admin.ModelAdmin):
+    search_fields = [
+        'machine', 'product',
+    ]
+    list_display = [
+        'machine', 'product', 'count',
     ]
--- backend/vending_machine/stock/models.py
+++ backend/vending_machine/stock/models.py
@@ -13,8 +13,8 @@ class Product(models.Model):
     code = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255)
     price = models.FloatField()
-    count = models.SmallIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    image = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
