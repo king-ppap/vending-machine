6b9c67498487db182834931cb1b9e8183b0bb982
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-09-22 02:25:37 +0700

    Update UI

--- frontend/vending_machine/components/machine/ItemProduct.tsx
+++ frontend/vending_machine/components/machine/ItemProduct.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function ItemProduct(props: Props) {
     return props.item.count > 0 ? (
-        <div className="p-3 rounded-2xl flex flex-col justify-center items-center bg-[#ffffff89] text-white">
+        <div className="p-3 rounded-2xl flex flex-col justify-center items-center bg-[#ffffff50] text-white">
             <div className='text-center'>
                 <img
                     className="w-[100px] h-[100px] object-cover"
